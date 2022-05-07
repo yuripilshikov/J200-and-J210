535e86ef0d7009135265f2b00590872a84d533a8
Yuri Pilshikov <yuri.nikolaevitch@gmail.com>
2022-05-07 09:32:21 +0300

    Model created

---
+++ J200_homework/src/java/model/Client.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ *
+ * @author YuriPilshikov
+ */
+public class Client {
+    private int idClient;   // 100 characters
+    private String type;    // 100 characters
+    private String model;   // 100 characters
+    private String ip;      // 25 characters
+}
