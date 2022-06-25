51b64e97cc2123a8415832c2cccaad0c9fb54c3e
Yuri Pilshikov <yuri.nikolaevitch@gmail.com>
2022-06-25 10:46:31 +0300

    Finished. But cannot delete client...

--- J200_HW_part2/src/java/rs/service/AbstractFacade.java
+++ J200_HW_part2/src/java/rs/service/AbstractFacade.java
@@ -32,6 +32,7 @@ public abstract class AbstractFacade<T> {
 
     public void remove(T entity) {
         getEntityManager().remove(getEntityManager().merge(entity));
+        getEntityManager().flush(); ////////////////
     }
 
     public T find(Object id) {
